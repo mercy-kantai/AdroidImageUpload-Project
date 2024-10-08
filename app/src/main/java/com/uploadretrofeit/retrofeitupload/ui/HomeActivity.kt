fe354a6389dc15911e26fbbc21e61196a87c9a69
mercy-kantai <kantaiemilymercy@gmail.com>
2024-10-08 16:53:08 +0300

    added changes

--- app/src/main/java/com/uploadretrofeit/retrofeitupload/ui/HomeActivity.kt
+++ app/src/main/java/com/uploadretrofeit/retrofeitupload/ui/HomeActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.uploadretrofeit.retrofeitupload.R
 import com.uploadretrofeit.retrofeitupload.databinding.ActivityHomeBinding
 
@@ -16,4 +17,39 @@ class HomeActivity : AppCompatActivity() {
         setContentView(binding.root)
 
     }
+
+    override fun onResume() {
+        super.onResume()
+        binding.btnvHome.setOnItemSelectedListener { menuItem->
+            when(menuItem.itemId){
+                R.id.home->{
+                    loadFragment(HomeFragment())
+                }
+                R.id.games->{
+                    loadFragment(GamesFragment())
+                }
+                R.id.profile->{
+                    loadFragment(ProfileFragment())
+                }
+                R.id.settings->{
+                    loadFragment(SettingsFragment())
+                }
+                else -> {
+                    loadFragment(HomeFragment())
+                }
+
+            }
+
+
+
+        }
+    }
+
+    fun loadFragment(fragment: Fragment): Boolean{
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fcvHome, fragment)
+        fragmentTransaction.commit()
+        return true
+    }
+
 }
